8d81b2deae446ffdd9546f4843d99f36c4ca9874
markotikvic <markotikvic@gmail.com>
2017-01-20 21:19:09 +0100

    inputEvent(e) proverava da li je doslo do eventa e. Esc x1 pauzira igru; -> nastavlja igru; Esc x2 izlazi u glavni meni.

--- ApatorQuest/src/inputManager.js
+++ ApatorQuest/src/inputManager.js
@@ -1,8 +1,9 @@
-
+var _inputEvents = [];
 var inputManagerUp = Keyboard(38);
 var inputManagerDown = Keyboard(40);
 var inputManagerLeft = Keyboard(37);
 var inputManagerRight = Keyboard(39);
+var inputManagerEsc = Keyboard(27);
 
 /*inputManagerUp.press = function () {
     //console.log('Up press');
@@ -35,3 +36,33 @@ inputManagerRight.press = function () {
 inputManagerRight.release = function () {
     //console.log('Right relese');
 };*/
+
+// Returns true if e.code is in input array.
+// TODO: Add checking for actions (keydown, keyup etc.)?
+function inputEvent(e) {
+    for (var i = 0; i < _inputEvents.length; i++) {
+        if (e.code == _inputEvents[i]) {
+            _inputEvents.splice(i, 1);
+            return true;
+        }
+    }
+    return false;
+}
+
+// Logs all user input (except for browser shortcuts).
+function keylogger(e) {
+    k = e.keyCode;
+    for (var i = 0; i < _inputEvents.length; i++) {
+        if (k == _inputEvents[i])
+            return;
+    }
+    _inputEvents.push(k);
+} 
+
+// Forgets all user input.
+// TODO: Test browser compatibility.
+function forgetInput() {
+    _inputEvents.length = 0;
+}
+
+document.addEventListener("keydown", keylogger);
